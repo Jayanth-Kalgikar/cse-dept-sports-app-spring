c7845fe370d6a5f8c4c722d2c3d67a93bfed295c
Jayanth Kalgikar <74163955+Jayanth-Kalgikar@users.noreply.github.com>
2025-06-08 13:11:54 +0530

    Delete src/main/java/com/app/sports/repo/SpoRepo.java

--- src/main/java/com/app/sports/repo/SpoRepo.java
+++
@@ -1,9 +0,0 @@
-package com.app.sports.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.app.sports.entity.SpoEntity;
-
-public interface SpoRepo extends JpaRepository<SpoEntity, Integer> {
-	
-	SpoEntity findByEventName(String eventName);
-}
